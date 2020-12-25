ddcbb813397ccefd1a7a01b7e119085db6059aa9
leefordjudes <leefordjudes@gmail.com>
2020-12-25 22:28:14 +0530

    u

--- src/app/observable/share-replay/share-replay.component.ts
+++ src/app/observable/share-replay/share-replay.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-replay.component.scss']
 })
 export class ShareReplayComponent implements OnInit {
-  url = 'https://test-products-b05fe.firebaseio.com/products.json';
+  // url = 'https://test-products-b05fe.firebaseio.com/products.json';
+  url = "https://my-json-server.typicode.com/leefordjudes/rxjsSeries/products";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(this.url).subscribe(res => {
+      console.log(res);
+    });
   }
 
 }
